85a100fd151282ed5d7e54f92a045e922f5216fe
YZD <yinzhendong@zhongledp.com>
2016-10-19 17:28:53 +0800

    修复启动白屏问题

---
+++ app/src/main/java/com/done/extractapk/view/TwoEndsProgress.java
@@ -0,0 +1,23 @@
+package com.done.extractapk.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by YZD on 2016/10/19.
+ */
+
+public class TwoEndsProgress extends RelativeLayout {
+    public TwoEndsProgress(Context context) {
+        super(context);
+    }
+
+    public TwoEndsProgress(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TwoEndsProgress(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
